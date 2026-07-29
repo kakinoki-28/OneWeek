05c30eaa4d7bc7d9f7e0ac683e58af62ed1272a4
kakinoki-28 <kakinoki.kaki@gmail.com>
2026-07-29 17:48:57 +0900

    feat: カメラ制御スクリプトの追加
    操作後に俯瞰視点にカメラの動かす用のスクリプトを追加

--- Assets/Project/Scenes/miya/CameraControlScript.cs
+++ Assets/Project/Scenes/miya/CameraControlScript.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class CameraControlScript : MonoBehaviour
 {
+    [SerializeField] private Vector3 goalPosition = new Vector3(-22, 20, 10);
+    [SerializeField] private Quaternion goalRotation = Quaternion.Euler(25, 115, 0);
+    [SerializeField] private float RotateTime = 1.0f;
+
+    private Vector3 currentVelocity;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -9,8 +14,20 @@ public class CameraControlScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        this.transform.position = Vector3.SmoothDamp(
+            this.transform.position,
+            goalPosition,
+            ref currentVelocity,
+            RotateTime
+        );
+
+        this.transform.rotation = Quaternion.Slerp(
+            this.transform.rotation,
+            goalRotation,
+            1f-Mathf.Exp(-Time.deltaTime / RotateTime)
+        );
+
     }
 }
---
+++ Assets/Project/Scenes/miya/ShowTransformScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ShowTransformScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Rotation:"+this.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
